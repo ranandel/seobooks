ef8d1ff9977a184be68b7b5eb688547dace20cb8
Yauhen <simbios2014@mail.ru>
2021-01-03 22:36:53 +0300

    03.01.2021

--- index.php
+++ index.php
@@ -8,18 +8,16 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
+<!--ШАПКА-->
 <div class="header_block">
-
     <header>
         <a href="/"><img src="images/logo2021_1.png" no-repeat></a>
-
+        <!--РЕКЛАМА-->
         <div class="adv">
         <a href="#"><img src="images/adv.png" no-repeat></a>
         </div>
-
 </header>
-
+    <!--МЕНЮ-->
 <menu>
     <ul>
         <li><a href="/">ГЛАВНАЯ</a></li>
@@ -30,14 +28,43 @@
         <li><a href="faq">ПОМОЩЬ</a></li>
     </ul>
 </menu>
-
 </div>
-
+<!--КОНТЕНТ-->
 <div class="content_block">
-    <div class="left">1</div>
+    <!--ЛЕВАЯ КОЛОНКА-->
+    <div class="left">
+<div class="statistika">
+    Мы работаем<br>
+    11 лет 9 месяцев 7 дней
+</div>
+        <div class="statistika">
+            Всего участников<br>
+            3 206 379
+        </div>
+        <div class="statistika">
+            Новых за 24 часа<br>
+            2316
+        </div>
+        <div class="statistika">
+            Мы выплатили<br>
+            100 230 864.00 руб.
+        </div>
+        <a href="#"><div class="reg"></div></a>
+        <a href="#"><div class="login"></div></a>
+        <div class="baner">
+            <h1>Реклама (Баннер 100х100)</h1>
+            <div class="baner_content">
+                <a href="#"><img src="images/baner.png" no-repeat></a>
+            </div>
+            <div class="ref">
+            <a href="#">Разместить</a>  | <a href="#">Смотреть все</a>
+            </div>
+        </div>
+    </div>
+    <!--СРЕДНЯЯ КОЛОНКА-->
     <div class="content">2</div>
+    <!--ПРАВАЯ КОЛОНКА-->
     <div class="right">3</div>
 </div>
-
 </body>
 </html>
