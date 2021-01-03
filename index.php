cd06f29d01c38ea7409723e1747347bf13eed624
Yauhen <simbios2014@mail.ru>
2021-01-03 19:47:37 +0300

    03.01.2021

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>ProfiTCentR - Рекламное агентство. Эффективная раскрутка сайтов</title>
+    <link rel="icon" href="/favicon.ico">
+    <script src="script.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="header_block">
+
+    <header>
+        <a href="/"><img src="images/logo2021_1.png" no-repeat></a>
+
+        <div class="adv">
+        <a href="#"><img src="images/adv.png" no-repeat></a>
+        </div>
+
+</header>
+
+<menu>
+    <ul>
+        <li><a href="/">ГЛАВНАЯ</a></li>
+        <li><a href="earn">ЗАРАБОТОК</a></li>
+        <li><a href="advertise">ЗАКАЗ РЕКЛАМЫ</a></li>
+        <li><a href="news">НОВОСТИ</a></li>
+        <li><a href="tos">ПРАВИЛА</a></li>
+        <li><a href="faq">ПОМОЩЬ</a></li>
+    </ul>
+</menu>
+
+</div>
+
+<div class="content_block">
+    <div class="left">1</div>
+    <div class="content">2</div>
+    <div class="right">3</div>
+</div>
+
+</body>
+</html>
