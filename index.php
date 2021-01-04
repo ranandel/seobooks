a7a1184b2aa06a080f9afc4cae0ba2ed30355114
Yauhen <simbios2014@mail.ru>
2021-01-05 01:14:34 +0300

    04.01.2021

--- index.php
+++ index.php
@@ -8,16 +8,33 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+<!--ЛЮДЕЙ ОНЛАЙН-->
+<div class="online" title="Сколько человек онлайн">
+    <span>1</span>
+</div>
+<!--ПОМОЩЬ-->
+<div class="help" >
+    <a href="#"><span class="x" title="Скрыть"> X</span></a>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="#"> Помощь</a>
+</div>
+<!--ТЕХПОДДЕРЖКА-->
+<div class="help" id="support">
+    <a href="#"><span class="x" title="Скрыть"> X</span></a>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="#"> Техподдержка</a>
+</div>
 <!--ШАПКА-->
 <div class="header_block">
     <header>
-        <a href="/"><img src="images/logo2021_1.png" no-repeat></a>
-        <!--РЕКЛАМА-->
+<!--ЛОГОТИП-->
+        <a href="/"><img src="images/logo.png" no-repeat></a>
+<!--РЕКЛАМА 480х70-->
         <div class="adv">
         <a href="#"><img src="images/adv.png" no-repeat></a>
         </div>
-</header>
-    <!--МЕНЮ-->
+    </header>
+<!--МЕНЮ-->
 <menu>
     <ul>
         <li><a href="/">ГЛАВНАЯ</a></li>
@@ -29,42 +46,206 @@
     </ul>
 </menu>
 </div>
-<!--КОНТЕНТ-->
+<!--КОНТЕНТ БЛОК 3 КОЛОНКИ-->
 <div class="content_block">
-    <!--ЛЕВАЯ КОЛОНКА-->
+<!--ЛЕВАЯ КОЛОНКА-->
     <div class="left">
+<!--СТАТИТИКА-->
 <div class="statistika">
-    Мы работаем<br>
-    11 лет 9 месяцев 7 дней
+    Мы работаем<br><span class="st">0</span> лет <span class="st">0</span> месяцев <span class="st">1</span> дней
 </div>
         <div class="statistika">
-            Всего участников<br>
-            3 206 379
+            Всего участников<br><span class="st">1</span>
         </div>
         <div class="statistika">
-            Новых за 24 часа<br>
-            2316
+            Новых за 24 часа<br><span class="st">1</span>
         </div>
         <div class="statistika">
-            Мы выплатили<br>
-            100 230 864.00 руб.
+            Мы выплатили<br><span class="st">0</span> руб.
         </div>
-        <a href="#"><div class="reg"></div></a>
-        <a href="#"><div class="login"></div></a>
+<!--РЕГИСТРАЦИЯ-->
+        <a href="1"><div class="reg"></div></a>
+<!--ВХОД-->
+        <a href="2"><div class="login"></div></a>
+<!--БАНЕР 100х100-->
         <div class="baner">
             <h1>Реклама (Баннер 100х100)</h1>
             <div class="baner_content">
                 <a href="#"><img src="images/baner.png" no-repeat></a>
             </div>
-            <div class="ref">
+            <div class="banner_bt">
             <a href="#">Разместить</a>  | <a href="#">Смотреть все</a>
             </div>
         </div>
     </div>
-    <!--СРЕДНЯЯ КОЛОНКА-->
-    <div class="content">2</div>
+<!--СРЕДНЯЯ КОЛОНКА-->
+    <div class="content">
+        <div class="name_block">
+<h1 class="zagolovok">Рекламодателям</h1>
+        <div class="content_zagolovok">
+            PROFITCENTR - РЕКЛАМНОЕ АГЕНТСТВО №1. БЫСТРО, ДОСТУПНО, КАЧЕСТВЕННО!
+        </div>
+        <div class="content_text">
+            <table >
+                <tr>
+                    <td colspan="2">
+                        <img src="images/favorite.png">  <span>Многофункциональный сервис заданий</span><br>
+                        Реальные люди выполнят любые ваши задания
+                    </td>
+                </tr>
+                <tr>
+                    <td width="65px">
+                        <img class="big_img" src="images/task.png">
+                    </td>
+                    <td>Расширенные настройки таргетинга и интервалов.<br>
+                        Автоматическая расшифровка GCLID кода в отчете.<br>
+                        Целевая аудитория и качественные выполнения.<br>
+                        Ручная или автоматическая проверка отчетов.<br><br>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <img src="images/favorite.png">  <span>Продвижение сайтов в поисковых системах</span><br>
+                        Реальные исполнители найдут сайт по Вашим поисковым запросам<br>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="65px">
+                        <img class="big_img" src="images/seo.png">
+                    </td>
+                    <td>Переходы на сайт со страниц Google или Яндекс.<br>
+                        Переходы на сайт по ключевым словам.<br>
+                        Клики по рекламе на Вашем сайте.<br>
+                        Возможность включения Эксперт режима.<br><br>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <img src="images/favorite.png">  <span>Продвижение YouTube видео-каналов</span><br>
+                        Огромная аудитория реальных людей с удовольствием готова:<br>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="65px">
+                        <img class="big_img" src="images/youtube.png">
+                    </td>
+                    <td>Посмотреть Ваше видео.<br>
+                        Подписаться на Ваш YouTube канал.<br>
+                        Поставить лайк Вашему видео.<br>
+                        Автоматическая проверка выполнений.<br><br>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <img src="images/favorite.png">  <span>Продвижение групп ВКонтакте</span><br>
+                        Десятки тысяч реальных людей захотят:<br>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="65px">
+                        <img class="big_img" src="images/vk.png">
+                    </td>
+                    <td>Вступить в Вашу группу.<br>
+                        Рассказать о ней друзьям.<br>
+                        Создать пост о ней, поставить лайк.<br>
+                        Автоматическая проверка выполнений.<br><br>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <img src="images/favorite.png">  <span>Продвижение профилей Instagram</span><br>
+                        Реальная живая аудитория:<br>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="65px">
+                        <img class="big_img" src="images/insta.png">
+                    </td>
+                    <td>Подпишется на Ваш профиль.<br>
+                        Поставит лайк на публикацию.<br>
+                        Автоматическая проверка выполнений.<br>
+                    </td>
+                </tr>
+            </table>
+            <br>
+            <img src="images/favorite.png"><span>Серфинг</span> - Десятки тысяч людей посетят Ваш сайт.<br>
+            <img src="images/favorite.png"><span>Письма </span>- Прочтение письма + просмотр сайта.<br>
+            <img src="images/favorite.png"><span>Контекстная реклама</span> - Только целевая аудитория.<br>
+            <img src="images/favorite.png"><span>Баннерная реклама</span> - Безлимитное кол-во переходов.<br>
+            <img src="images/favorite.png"><span>Каталог ссылок </span>- Повышение тИЦ Вашего ресурса.<br>
+            <br>
+            <div style="text-align: center;">
+            <img src="images/pay.png" style="opacity:0.4;" >
+            </div>
+            <br>
+            <div style="text-align: center;">
+              <a href="#" class="hochy">Конечно хочу</a>
+                <br><br>
+            </div>
+        </div>
+    </div>
+        <br>
+
+        <div class="name_block">
+        <div class="content" style="margin: 0;">
+            <h1 class="zagolovok">Пользователям - Деньги</h1>
+            <div class="content_zagolovok">
+                ПОЛУЧАЙТЕ ДЕНЬГИ ЗА ТО, ЧТО ВСЕГДА ДЕЛАЛИ БЕСПЛАТНО!
+            </div>
+            <div class="content_text">
+                <table >
+                    <tr>
+                        <td style="padding-right: 70px">
+            <img src="images/favorite.png"><span>Выполняй задания</span> - Получай деньги.<br>
+            <img src="images/favorite.png"><span>Вступай в группы </span>- Получай деньги.<br>
+            <img src="images/favorite.png"><span>Выполняй тесты </span> - Получай деньги.<br>
+            <img src="images/favorite.png"><span>Делай репосты</span> - Получай деньги.<br>
+            <img src="images/favorite.png"><span>Смотри сайты </span>- Получай деньги.<br>
+            <img src="images/favorite.png"><span>Смотри видео </span>- Получай деньги.<br>
+            <img src="images/favorite.png"><span>Читай письма </span>- Получай деньги.<br>
+            <img src="images/favorite.png"><span>Ставь лайки </span>- Получай деньги.<br>
+            <img src="images/favorite.png"><span>Зови друзей </span>- Получай деньги.<br>
+                        </td>
+                        <td align="right">
+                <img src="images/money.jpg">
+                        </td>
+                    </tr>
+                </table>
+            <br>
+                <div style="text-align: center;">
+                    <img src="images/pay2.png" style="opacity:0.4;" >
+                </div>
+                <br>
+                <div style="text-align: center;">
+                    <a href="#" class="hochy">Хочу денег</a>
+                    <br><br>
+                </div>
+        </div>
+        </div>
+        </div>
+        <br>
+    </div>
     <!--ПРАВАЯ КОЛОНКА-->
-    <div class="right">3</div>
+    <div class="right" >
+        <div class="name_block">
+            <h1 class="zagolovok">Контекстная реклама</h1>
+            <div class="link">
+                <br><br><br><hr>
+                <br><br><br> <hr>
+                <br><br><br><hr>
+                <br><br><br><hr>
+                <br><br><br><hr>
+                <br><br><br><hr>
+            </div>
+            <div class="banner_bt" id="link_bt">
+                <a href="#">Разместить</a>  | <a href="#">Смотреть все</a>
+            </div>
+        </div>
+        <br>
+<div class="adv_right">
+<a href="#"> <img src="images/adv2.png"></a>
+</div>
+    </div>
 </div>
 </body>
 </html>
